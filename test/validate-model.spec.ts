2a7142d9c547737a498bbb47e9d3aa0e3dd84e63
wk-y <86206045+wk-y@users.noreply.github.com>
2026-04-24 08:14:25 -0700

    remove requirement for links to source code for now

--- test/validate-model.spec.ts
+++ test/validate-model.spec.ts
@@ -1,17 +1,6 @@
 import { test } from 'vitest'
 import { likec4model } from './likec4-model'
 
-test('Every service has a link to the repository', ({ expect }) => {
-  expect.hasAssertions()
-  for (const e of likec4model.elementsOfKind('service')) {
-    // expect.soft allows to accumulate all failures
-    expect.soft(
-      e.links.find(l => l.title === 'Repository'),
-      `Service "${e.id}" has no link to the repository`
-    ).toBeDefined()
-  }
-})
-
 test('Many-to-many relationship allowed only between DB Tables', ({ expect }) => {
   for (const r of likec4model.relationshipsWhere({ kind: 'many-to-many' })) {
     expect.soft(
